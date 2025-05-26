82d8a0db6d0052994f485f8135145e2809327d73
Daniel Zhao <jumpyzhao@gmail.com>
2025-05-26 19:10:48 +1000

    Updated header component to assysts brand name

--- apps/web/app/[locale]/components/header/index.tsx
+++ apps/web/app/[locale]/components/header/index.tsx
@@ -122,7 +122,7 @@ export const Header = ({ dictionary }: HeaderProps) => {
             height={24}
             className="dark:invert"
           />
-          <p className="whitespace-nowrap font-semibold">next-forge</p>
+          <p className="whitespace-nowrap font-semibold">assysts</p>
         </div>
         <div className="flex w-full justify-end gap-4">
           <Button variant="ghost" className="hidden md:inline" asChild>
